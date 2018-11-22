325e8b61aee18f681a1800b6d25e57b311c74964
Amelia Roseto <roseto@chapman.edu>
2018-11-22 19:09:26 +0000

    trying

--- ode.py
+++ ode.py
@@ -12,53 +12,63 @@
 import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
-xRange = np.arange(0, 10*np.pi, 2*np.pi/N)
-I = np.matrix('0 1; -1 0')
-n = 1
-change = xRange[1] - xRange[0]
+I = np.array([[0,1],[-1,0]])
 
 def euler_1(initP, change):
     slope = I @ initP
     return initP + (change * slope)
 
 def euler_2(N, u):
-    eulerApprox = np.zeros(len(xRange)+1)
+    xRange = np.arange(0, 10*np.pi, 2*np.pi/N)
+    eulerApprox = np.zeros((len(xRange)+1, 2))
+    change = xRange[1] - xRange[0]
     eulerApprox[0] = u
+    n = 0
     for x in xRange:
+        n += 1
         eulerApprox[n] = euler_1(eulerApprox[n-1], change)
     return eulerApprox
 
 def Heuns_1(initP, change):
     Approx_2 = euler_1(initP, change)
-    return initP + ((change/2)*(I @ (initP + Approx_2)))
+    return initP + ((change/2)*(I * (initP + Approx_2)))
 
 def Heuns_2(N, u):
-    heunApprox = np.zeros(len(xRange)+1)
+    xRange = np.arange(0, 10*np.pi, 2*np.pi/N)
+    change = xRange[1] - xRange[0]
+    heunApprox = np.zeros((len(xRange)+1, 2))
     heunApprox[0] = u
+    n = 0
     for x in xRange:
-        heunApprox[n] = Heun_1(heunApprox[n-1], change)
+        n += 1
+        heunApprox[n] = Heuns_1(heunApprox[n-1], change)
     return heunApprox
 
 def rungeKuttaSecond_1(initP, change):
     k1 = change*(I @ initP)
-    k2 = change*(I @ (initP + (k1 / 2))
+    k2 = change*(I @ (initP + (k1 / 2)))
     return initP + k2
 
 def rungeKuttaSecond_2(N, u):
+    xRange = np.arange(0, 10*np.pi, 2*np.pi/N)
+    change = xRange[1] - xRange[0]
     RKSApprox = np.zeros((len(xRange)+1, 2))
     RKSApprox[0] = u
+    n = 0
     for x in xRange:
         RKSApprox[n] = rungeKuttaSecond_1(RKSApprox[n-1], change)
     return RKSApprox
 
 def rungeKuttaFourth_1(initP, change):
     k1 = change*(I @ initP)
-    k2 = change*(I @ (initP + (k1 / 2))
-    k3 = change*(I @ (initP + (k2 / 2))
-    k4 = change*(I @ (initP + k3)
+    k2 = change*(I @ (initP + (k1 / 2)))
+    k3 = change*(I @ (initP + (k2 / 2)))
+    k4 = change*(I @ (initP + k3))
     return initP + (k1 + 2*k2 + 2*k3 + k4)/6
 
 def rungeKuttaFourth_2(N, u):
+    xRange = np.arange(0, 10*np.pi, 2*np.pi/N)
+    change = xRange[1] - xRange[0]
     RKSApprox = np.zeros((len(xRange)+1, 2))
     RKSApprox[0] = u
     n = 0
